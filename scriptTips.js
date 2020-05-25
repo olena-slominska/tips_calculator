59701a5390a18856a449b42b4c23784307a033cd
olena-slominska <petrushe4ka@gmail.com>
2020-05-25 17:22:03 +0300

    corrected rounding for Tip field

--- scriptTips.js
+++ scriptTips.js
@@ -1,7 +1,7 @@
 // code using jQuery
 $(document).on('click check',"#cont1", function() {
   tips = eval(bill.value*percent.value/100/pplnmbr.value);
-  document.getElementById('tipresult').innerHTML = " $" + Math.round(tips).toFixed(2); 
+  document.getElementById('tipresult').innerHTML = " $" + Number(tips).toFixed(2); 
   total = eval(bill.value/pplnmbr.value + tips);
   document.getElementById('totalresult').innerHTML = " $" + Number(total).toFixed(2);
   if (pplnmbr.value > 1) {
@@ -12,7 +12,7 @@ $(document).on('click check',"#cont1", function() {
 // pure JS code
 document.addEventListener('click',function() {
   tips2 = eval(bill2.value*percent2.value/100/pplnmbr2.value);
-  document.getElementById('tipresult2').innerHTML = " $" + Math.round(tips2).toFixed(2); 
+  document.getElementById('tipresult2').innerHTML = " $" + Number(tips2).toFixed(2); 
   total2 = eval(bill2.value/pplnmbr2.value + tips2);
   document.getElementById('totalresult2').innerHTML = " $" + Number(total2).toFixed(2);
   if (pplnmbr2.value > 1) {
